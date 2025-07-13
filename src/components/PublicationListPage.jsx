1d85bca0cd8c5fcf904887cc88af837ddc3ac510
nailatte <n240305@gmail.com>
2025-07-13 19:56:10 +0700

    detail

--- src/components/PublicationListPage.jsx
+++ src/components/PublicationListPage.jsx
@@ -17,7 +17,7 @@ export default function PublicationListPage() {
   };
 
   const handleViewDetail = (id) => {
-    navigate(`/publications/detail/${id}`);
+    navigate(`/publications/${publication.id}`)
   };
 
   return (
